13132cdf4e4ad81c86133a5ed66e72c3d95a175a
Ray <lc3202683603@gmail.com>
2018-12-31 16:33:23 +0800

    终极版本+命令+Jar

---
+++ workspace/NB_2016082074/src/NBMain.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import com.alibaba.fastjson.JSON;
+
+
+
+public class NBMain
+{
+    
+	public static void main(String[] args) throws Exception
+	{	
+		BayesPredict predictor = new BayesPredict(args);
+		
+		predictor.InitConfiguration();	// 初始化
+		
+//		predictor.GetFrequencyFile();	// 得到频数文件
+		
+		predictor.readMapFromHdfs("hdfs://node:9000/user/Hadoop/eclipseOutput2/part-r-00000");
+		predictor.Predict();			// 开始预测
+		predictor.GetAccurateFromPredictJob();	// 输出预测信息
+	}
+
+}
